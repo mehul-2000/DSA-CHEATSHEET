6cc9ace83df346413febeae6d8644f2fda831b2a
Mehul <gulatimehul2000@gmail.com>
2022-01-07 21:14:52 +0530

    commonElements.cpp added

---
+++ commonElements.cpp
@@ -0,0 +1,62 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+//  Written by :- Mehul Kumar
+ // } Driver Code Ends
+class Solution
+{
+    public:    
+    
+       vector <int> commonElements (int A[], int B[], int C[], int n1, int n2, int n3)
+        {
+            //code here.
+            int i=0,j=0,k=0;
+            vector<int> res;
+            map<int,bool> mp;
+            while(i<n1 && j<n2 && k<n3){
+                if(A[i]==B[j] && B[j]==C[k]){
+                    if(!mp[A[i]]){
+                    res.push_back(A[i]);
+                    mp[A[i]]=true;
+                    }
+                    i++;j++;k++;
+                }
+                else{
+                    int min_ele = min(A[i],B[j],C[k]);
+                    if(min_ele == A[i]){i++;}
+                    else if(min_ele == B[j]){j++;}
+                    else if(min_ele == C[k]){k++;}
+                }
+            }
+            return res;
+        }
+
+};
+
+// { Driver Code Starts.
+
+int main ()
+{
+    int t; cin >> t;
+    while (t--)
+    {
+        int n1, n2, n3; 
+        cin >> n1 >> n2 >> n3;
+        int A[n1];
+        int B[n2];
+        int C[n3];
+        
+        for (int i = 0; i < n1; i++) cin >> A[i];
+        for (int i = 0; i < n2; i++) cin >> B[i];
+        for (int i = 0; i < n3; i++) cin >> C[i];
+        
+        Solution ob;
+        
+        vector <int> res = ob.commonElements (A, B, C, n1, n2, n3);
+        if (res.size () == 0) 
+            cout << -1;
+        for (int i = 0; i < res.size (); i++) 
+            cout << res[i] << " "; 
+        cout << endl;
+    }
+}  // } Driver Code Ends
